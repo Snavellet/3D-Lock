45b8ab0935d975de2e11fe19e0cc99db854c4a29
Colin Saari <snavellet@gmail.com>
2020-04-16 23:15:45 +0700

    Optimized code and updated package.json

--- listeners/commands/warn.js
+++ listeners/commands/warn.js
@@ -3,154 +3,153 @@ const WarningThreshold = require('../../models/warningThresholdModel');
 const Blacklist = require('../../models/blackListModel');
 
 module.exports = {
-    name: 'warn',
-    description: 'The purpose of this command is to warn.',
-    args: true,
-    cooldown: 5,
-    async execute(message, args) {
-        if(!message.member.hasPermission('KICK_MEMBERS')) return;
+	name: 'warn',
+	description: 'The purpose of this command is to warn.',
+	args: true,
+	cooldown: 5,
+	async execute(message, args) {
+		if(!message.member.hasPermission('KICK_MEMBERS')) return;
 
-        const matchedIDs = message.content.match(/(\s+)(\d+)/g);
-        const noMentions = 'please mention someone to warn!';
-        let reason;
+		const matchedIDs = message.content.match(/(\s+)(\d+)/g);
+		const noMentions = 'please mention someone to warn!';
+		let reason;
 
-        const warn = async (userArr, userArrIDs, reason) => {
-            const realWarnMentions = async userNewArr => {
-                for(const user in userNewArr) {
-                    const guildMember = message.guild.members.get(userNewArr[user].id);
-                    if(guildMember.hasPermission('KICK_MEMBERS')) return await message.reply('you cannot warn a mod!');
+		const warn = async (userArr, userArrIDs, reason) => {
+			const realWarnMentions = async userNewArr => {
+				for(const user in userNewArr) {
+					const guildMember = message.guild.members.get(userNewArr[user].id);
+					if(guildMember.hasPermission('KICK_MEMBERS')) return await message.reply('you cannot warn a mod!');
 
-                    if(!guildMember) {
-                        await message.reply(`${userNewArr[user].id} doesnt exist in this server anymore!`);
-                        continue;
-                    }
+					if(!guildMember) {
+						await message.reply(`${userNewArr[user].id} doesnt exist in this server anymore!`);
+						continue;
+					}
 
-                    let warning = await Warning.findOne({ guildID: guildMember.guild.id, userID: guildMember.id });
-                    if(!warning) {
-                        warning = await Warning.create({
-                            guildID: message.guild.id,
-                            guildName: message.guild.name,
-                            userID: guildMember.user.id,
-                            userTag: guildMember.user.tag,
-                            warnings: process.env.WARNING_INITIAL * 1
-                        });
-                    } else {
-                        warning = await Warning.findOneAndUpdate({
-                            guildID: message.guild.id,
-                            userID: guildMember.user.id
-                        }, {
-                            guildName: message.guild.name,
-                            userTag: guildMember.user.tag,
-                            warnings: warning.warnings + 1
-                        }, { upsert: true, new: true })
-                    }
+					let warning = await Warning.findOne({ guildID: guildMember.guild.id, userID: guildMember.id });
+					if(!warning) {
+						warning = await Warning.create({
+							guildID: message.guild.id,
+							guildName: message.guild.name,
+							userID: guildMember.user.id,
+							userTag: guildMember.user.tag,
+							warnings: process.env.WARNING_INITIAL * 1
+						});
+					} else {
+						warning = await Warning.findOneAndUpdate({
+							guildID: message.guild.id,
+							userID: guildMember.user.id
+						}, {
+							guildName: message.guild.name,
+							userTag: guildMember.user.tag,
+							warnings: warning.warnings + 1
+						}, { upsert: true, new: true })
+					}
 
-                    let warningThreshold = await WarningThreshold.findOne({ guildID: message.guild.id });
-                    if(!warningThreshold) return await message.reply('cannot check for the warning threshold, please try again later!');
-                    warningThreshold = warningThreshold.threshold;
-                    warning = warning.warnings;
+					let warningThreshold = await WarningThreshold.findOne({ guildID: message.guild.id });
+					if(!warningThreshold) return await message.reply('cannot check for the warning threshold, please try again later!');
+					warningThreshold = warningThreshold.threshold;
+					warning = warning.warnings;
 
-                    if(warning.warnings === warningThreshold) {
-                        await message.channel.send(`<@${guildMember.user.id}>, you have reached \`${warning}\` warning(s) out of \`${warningThreshold}\`, you will be banned!`);
+					if(warning.warnings === warningThreshold) {
+						await message.channel.send(`<@${guildMember.user.id}>, you have reached \`${warning}\` warning(s) out of \`${warningThreshold}\`, you will be banned!`);
 
-                        await Blacklist.create({
-                            guildID: message.guild.id,
-                            guildName: message.guild.name,
-                            userID: guildMember.user.id,
-                            userTag: guildMember.user.tag,
-                            reason: reason.split(' ')[1]
-                        });
+						await Blacklist.create({
+							guildID: message.guild.id,
+							guildName: message.guild.name,
+							userID: guildMember.user.id,
+							userTag: guildMember.user.tag,
+							reason: reason.split(' ')[1]
+						});
 
-                        return await guildMember.ban({
-                            reason
-                        });
-                    }
+						return await guildMember.ban({
+							reason
+						});
+					}
 
-                    await message.channel.send(`<@${guildMember.user.id}>, you have \`${warning}\` warning(s) out of \`${warningThreshold}\` for \`${reason.split(' ')[1]}\``);
-                }
-            };
+					await message.channel.send(`<@${guildMember.user.id}>, you have \`${warning}\` warning(s) out of \`${warningThreshold}\` for \`${reason.split(' ')[1]}\``);
+				}
+			};
 
-            const realWarnIDs = async userNewIDsArray => {
-                for(const user in userNewIDsArray) {
-                    const guildMember = message.guild.members.get(userNewIDsArray[user]);
+			const realWarnIDs = async userNewIDsArray => {
+				for(const user in userNewIDsArray) {
+					const guildMember = message.guild.members.get(userNewIDsArray[user]);
 
-                    if(!guildMember) {
-                        await message.reply(`${userNewIDsArray[user]} doesnt exist in this server anymore!`);
-                        continue;
-                    }
+					if(!guildMember) {
+						await message.reply(`${userNewIDsArray[user]} doesnt exist in this server anymore!`);
+						continue;
+					}
 
-                    let warning = await Warning.findOne({ guildID: guildMember.guild.id, userID: guildMember.id });
-                    if(!warning) {
-                        warning = await Warning.create({
-                            guildID: message.guild.id,
-                            guildName: message.guild.name,
-                            userID: guildMember.user.id,
-                            userTag: guildMember.user.tag,
-                            warnings: process.env.WARNING_INITIAL * 1
-                        });
-                    } else {
-                        warning = await Warning.findOneAndUpdate({
-                            guildID: message.guild.id,
-                            userID: guildMember.user.id
-                        }, {
-                            guildName: message.guild.name,
-                            userTag: guildMember.user.tag,
-                            warnings: warning.warnings + 1
-                        }, { upsert: true, new: true })
-                    }
+					let warning = await Warning.findOne({ guildID: guildMember.guild.id, userID: guildMember.id });
+					if(!warning) {
+						warning = await Warning.create({
+							guildID: message.guild.id,
+							guildName: message.guild.name,
+							userID: guildMember.user.id,
+							userTag: guildMember.user.tag,
+							warnings: process.env.WARNING_INITIAL * 1
+						});
+					} else {
+						warning = await Warning.findOneAndUpdate({
+							guildID: message.guild.id,
+							userID: guildMember.user.id
+						}, {
+							guildName: message.guild.name,
+							userTag: guildMember.user.tag,
+							warnings: warning.warnings + 1
+						}, { upsert: true, new: true })
+					}
 
-                    let warningThreshold = await WarningThreshold.findOne({ guildID: message.guild.id });
-                    if(!warningThreshold) return await message.reply('cannot check for the warning threshold, please try again later!');
-                    warningThreshold = warningThreshold.threshold;
-                    warning = warning.warnings;
+					let warningThreshold = await WarningThreshold.findOne({ guildID: message.guild.id });
+					if(!warningThreshold) return await message.reply('cannot check for the warning threshold, please try again later!');
+					warningThreshold = warningThreshold.threshold;
+					warning = warning.warnings;
 
-                    if(warning === warningThreshold) {
-                        await message.channel.send(`<@${guildMember.user.id}>, you have reached \`${warning}\` warning(s) out of \`${warningThreshold}\`, you will be banned!`);
+					if(warning === warningThreshold) {
+						await message.channel.send(`<@${guildMember.user.id}>, you have reached \`${warning}\` warning(s) out of \`${warningThreshold}\`, you will be banned!`);
 
-                        await Blacklist.create({
-                            guildID: message.guild.id,
-                            guildName: message.guild.name,
-                            userID: guildMember.user.id,
-                            userTag: guildMember.user.tag,
-                            reason: reason.split(' ')[1]
-                        });
+						await Blacklist.create({
+							guildID: message.guild.id,
+							guildName: message.guild.name,
+							userID: guildMember.user.id,
+							userTag: guildMember.user.tag,
+							reason: reason.split(' ')[1]
+						});
 
-                        return await guildMember.ban({
-                           reason
-                        });
-                    }
+						return await guildMember.ban({
+							reason
+						});
+					}
 
-                    await message.channel.send(`<@${guildMember.user.id}>, you have \`${warning}\` warning(s) out of \`${warningThreshold}\` for \`${reason.split(' ')[1]}\``);
-                }
-            };
+					await message.channel.send(`<@${guildMember.user.id}>, you have \`${warning}\` warning(s) out of \`${warningThreshold}\` for \`${reason.split(' ')[1]}\``);
+				}
+			};
 
-            if(userArr[0]) {
-                return await realWarnMentions(userArr);
-            }
+			if(userArr[0]) {
+				return await realWarnMentions(userArr);
+			}
 
-            return await realWarnIDs(userArrIDs);
-        };
+			return await realWarnIDs(userArrIDs);
+		};
 
-        reason = `${message.author.tag}: reason not supplied.`;
+		reason = `${message.author.tag}: reason not supplied.`;
 
-        if(message.mentions.users.array().length >= 1) {
-            const mentionedArray = message.mentions.users.array();
-            const lastElArr = args.slice(-1).pop();
+		if(message.mentions.users.array().length >= 1) {
+			const mentionedArray = message.mentions.users.array();
+			const lastElArr = args.slice(-1).pop();
 
-            if(lastElArr && lastElArr.match(/^\w+/g)) reason = `${message.author.tag}: ${lastElArr}`;
+			if(lastElArr && lastElArr.match(/^\w+/g)) reason = `${message.author.tag}: ${lastElArr}`;
 
-            await warn(mentionedArray, false, reason);
-        }
-        else if(matchedIDs) {
-            const newMatchedIDs = [];
-            matchedIDs.forEach(el => newMatchedIDs.push(el.replace(/^\s+/g, '')));
-            const lastElArr = args.slice(-1).pop();
+			await warn(mentionedArray, false, reason);
+		} else if(matchedIDs) {
+			const newMatchedIDs = [];
+			matchedIDs.forEach(el => newMatchedIDs.push(el.replace(/^\s+/g, '')));
+			const lastElArr = args.slice(-1).pop();
 
-            if(lastElArr && lastElArr.match(/^\w+/g)) reason = `${message.author.tag}: ${lastElArr}`;
+			if(lastElArr && lastElArr.match(/^\w+/g)) reason = `${message.author.tag}: ${lastElArr}`;
 
-            await warn(false, newMatchedIDs, reason);
-        } else {
-           return await message.reply(noMentions);
-        }
-    },
+			await warn(false, newMatchedIDs, reason);
+		} else {
+			return await message.reply(noMentions);
+		}
+	},
 };
--- listeners/message.js
+++ listeners/message.js
@@ -8,53 +8,53 @@ const client = require('../app');
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync(`${__dirname}/commands`).filter(file => file.endsWith('.js'));
 
-for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
+for(const file of commandFiles) {
+	const command = require(`./commands/${file}`);
 
-    // set a new item in the Collection
-    // with the key as the command name and the value as the exported module
-    client.commands.set(command.name, command);
+	// set a new item in the Collection
+	// with the key as the command name and the value as the exported module
+	client.commands.set(command.name, command);
 }
 
 
 module.exports = catchAsyncMessage(async message => {
-    if(message.author.id === message.client.id) return;
-    if(message.channel.type === 'dm' || message.author.bot) return;
+	if(message.author.id === message.client.id) return;
+	if(message.channel.type === 'dm' || message.author.bot) return;
 
-    const prefix = await getPrefix(message.guild.id, message.guild.name);
+	const prefix = await getPrefix(message.guild.id, message.guild.name);
 
-    if(!message.content.startsWith(prefix)) return;
+	if(!message.content.startsWith(prefix)) return;
 
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const commandName = args.shift().toLowerCase();
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const commandName = args.shift().toLowerCase();
 
-    if (!client.commands.has(commandName)) return;
+	if(!client.commands.has(commandName)) return;
 
-    const command = client.commands.get(commandName);
+	const command = client.commands.get(commandName);
 
-    if (command.args && !args.length) {
-        return message.channel.send(`You didn't provide any arguments, <@${message.author.id}>!`);
-    }
+	if(command.args && !args.length) {
+		return message.channel.send(`You didn't provide any arguments, <@${message.author.id}>!`);
+	}
 
-    if (!cooldowns.has(command.name)) {
-        cooldowns.set(command.name, new Discord.Collection());
-    }
+	if(!cooldowns.has(command.name)) {
+		cooldowns.set(command.name, new Discord.Collection());
+	}
 
-    const now = Date.now();
-    const timestamps = cooldowns.get(command.name);
-    const cooldownAmount = (command.cooldown || 3) * 1000;
+	const now = Date.now();
+	const timestamps = cooldowns.get(command.name);
+	const cooldownAmount = (command.cooldown || 3) * 1000;
 
-    if (timestamps.has(message.author.id)) {
-        const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+	if(timestamps.has(message.author.id)) {
+		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
 
-        if (now < expirationTime) {
-            const timeLeft = (expirationTime - now) / 1000;
-            return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
-        }
-    }
+		if(now < expirationTime) {
+			const timeLeft = (expirationTime - now) / 1000;
+			return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+		}
+	}
 
-    timestamps.set(message.author.id, now);
-    setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+	timestamps.set(message.author.id, now);
+	setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
 
-    await command.execute(message, args);
+	await command.execute(message, args);
 });
