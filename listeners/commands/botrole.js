df54cb2a362de96df70fe89ff24ca1cfbb431dcc
Colin Saari <snavellet@gmail.com>
2020-04-25 13:17:28 +0700

    Made some optimizations

--- listeners/commands/botrole.js
+++ listeners/commands/botrole.js
@@ -2,23 +2,23 @@ const roleCreateAndUpdate = require('../../utils/createAndUpdateRoles');
 const roleSame = require('../../utils/roleSame');
 
 module.exports = {
-    name: 'botrole',
-    description: 'The purpose of this command is to set the role for giving new bots.',
-    args: true,
-    cooldown: 5,
-    async execute(message, args) {
-        if(!message.member.hasPermission('MANAGE_ROLES')) return;
+	name: 'botrole',
+	description: 'The purpose of this command is to set the role for giving new bots.',
+	args: true,
+	cooldown: 5,
+	async execute(message, args) {
+		if(!message.member.hasPermission('MANAGE_ROLES')) return;
 
-        let role = message.guild.roles.get(args[0]);
-        const event = 'bot';
+		let role = message.guild.roles.get(args[0]);
+		const event = 'bot';
 
-        if(!role) return await message.reply('invalid role ID, please try again.');
-        if(await roleSame(message.guild.id, event, role.id)) {
-            return await message.reply('that\'s the role that is currently set to!');
-        }
+		if(!role) return await message.reply('invalid role ID, please try again.');
+		if(await roleSame(message.guild.id, event, role.id)) {
+			return await message.reply('that\'s the role that is currently set to!');
+		}
 
-        role = await roleCreateAndUpdate(message.guild, role, event);
+		role = await roleCreateAndUpdate(message.guild, role, event);
 
-        await message.reply(`successfully set the role to <@&${role.roleID}>!`);
-    },
+		await message.reply(`successfully set the role to <@&${role.roleID}>!`);
+	},
 };
--- listeners/ready.js
+++ listeners/ready.js
@@ -4,43 +4,47 @@ const Prefix = require('../models/prefixModel');
 const logger = require('../utils/logger');
 
 module.exports = client => {
-    return async () => {
-        logger.info(`Logged in successfully as ${client.user.tag}.`);
+	return async () => {
+		logger.info(`Logged in successfully as ${client.user.tag}.`);
 
-        const mainServer = client.guilds.get(process.env.MAIN_SERVER);
-        let roleID = await Role.findOne({ guildID: mainServer.id, event: 'beforeVerification' });
-        roleID = roleID.roleID;
+		const mainServer = client.guilds.get(process.env.MAIN_SERVER);
+		let roleID = await Role.findOne({ guildID: mainServer.id, event: 'beforeVerification' });
+		roleID = roleID.roleID;
 
-        let prefix = await Prefix.findOne({ guildID: mainServer.id });
-        prefix = prefix.prefix;
+		let prefix = await Prefix.findOne({ guildID: mainServer.id });
+		prefix = prefix.prefix;
 
-        const stopBypass = async (allUsers, member) => {
-            const verificationCode = allUsers.updateVerificationCode();
-            await allUsers.save();
+		const stopBypass = async (allUsers, member) => {
+			const verificationCode = allUsers.updateVerificationCode();
+			await allUsers.save();
 
-            await member.setRoles([roleID], 'Bypassing Verification Is Restricted');
-            await member.user.send(`Please say \`${prefix}verify ${verificationCode}\` to verify again, remember this code ***expires*** in **${process.env.VERIFICATION_EXPIRE} minutes**.`);
-        };
+			await member.setRoles([roleID], 'Bypassing Verification Is Restricted');
+			await member.user.send(`Please say \`${prefix}verify ${verificationCode}\` to verify again, remember this code ***expires*** in **${process.env.VERIFICATION_EXPIRE} minutes**.`);
+		};
 
-        setInterval( async () => {
-            const allUsers = await User.find({ guildID: mainServer.id });
+		setInterval(async () => {
+			const allUsers = await User.find({ guildID: mainServer.id });
 
-            for(const i in allUsers) {
-                const member = mainServer.members.get(allUsers[i].userID);
-                if(!member) return;
-                if(member.roles.array().length > 2) {
-                    await stopBypass(allUsers[i], member);
-                    continue;
-                }
+			for(const i in allUsers) {
+				const member = mainServer.members.get(allUsers[i].userID);
+				if(!member) return;
+				if(member.roles.array().length > 2) {
+					await stopBypass(allUsers[i], member);
+					continue;
+				}
 
-                if(member.roles.some(role => role.id === roleID)) continue;
+				if(member.roles.some(role => role.id === roleID)) continue;
 
-                await stopBypass(allUsers[i], member);
-            }
+				await stopBypass(allUsers[i], member);
+			}
 
-        }, 5000);
+		}, 5000);
 
 
-        await client.user.setPresence( { status: 'online', game: {type: 'WATCHING', name: 'this server'}, afk: false } );
-    }
+		await client.user.setPresence({
+			status: 'online',
+			game: { type: 'WATCHING', name: 'this server' },
+			afk: false
+		});
+	}
 };
